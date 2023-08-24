3e49a356f12fb82a2cf944d5aa56024902175255
Stefan Claas <13186616+stefanclaas@users.noreply.github.com>
2023-08-24 15:47:42 +0200

    Add files via upload

---
+++ sm.go
@@ -0,0 +1,37 @@
+﻿package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "net/smtp"
+)
+
+func main() {
+    // we are authorizing as
+    from := "yourusername@danwin1210.de"
+
+    // user we are sending email to
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: sm recipient@domain.com < message_with_Headers.txt")
+        os.Exit(1)
+    }
+    to := os.Args[1]
+
+    // server we are authorized to send email through
+    host := "danwin1210.de"
+
+    auth := smtp.PlainAuth("", from, "yourpassword", host)
+
+    scanner := bufio.NewScanner(os.Stdin)
+    message := ""
+    for scanner.Scan() {
+        message += scanner.Text() + "\n"
+    }
+
+    if err := smtp.SendMail(host+":587", auth, from, []string{to}, []byte(message)); err != nil {
+        fmt.Println("Error SendMail: ", err)
+        os.Exit(1)
+    }
+    fmt.Println("Email Sent!")
+}
